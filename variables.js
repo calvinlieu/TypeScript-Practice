c61a8ae105808db9fccd465971592820a2a07dfe
Calvin <calvin.lieu@gmail.com>
2023-05-22 22:44:44 -0700

    Type Inference

---
+++ variables.js
@@ -0,0 +1,8 @@
+var movieTitle = "Amadeus";
+movieTitle = "Arrival";
+movieTitle.toUpperCase();
+var catLives = 9;
+catLives += 1;
+// catLives = "zero"
+var gameOver = false;
+gameOver = true;
--- variables.ts
+++ variables.ts
@@ -1,5 +1,21 @@
 let movieTitle: string = "Amadeus";
 movieTitle = "Arrival";
-movieTitle.toUpperCase(
-    +
-)
+movieTitle.toUpperCase();
+
+
+let catLives: number = 9;
+catLives += 1;
+// catLives = "zero"
+
+let gameOver: boolean = false;
+gameOver = true;
+
+
+//Type Inference: Let TypeScript assign the type for you. It can infer the type of the variable from what we assign it to.
+let tvShow = "Olive Kitteridge";
+tvShow = "The Other Two";
+// tvShow = false;
+
+let isFunny = false;
+isFunny = true;
+// isFunny = "sadsa"
